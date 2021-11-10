6179799eab9c88c1d6e5e2321d9fb96977dd1567
Khemi <khemi@Khemis-Laptop.local>
2021-11-10 11:18:30 +0000

    fifth iteration: calculation shows on screen

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//variables (sourced from .html)
 const screen = document.querySelector("#screen");
+const calc = document.querySelector("#calculations");
 const buttons = document.querySelectorAll(".keys__orange, .keypad, .keys__orange_bg");
-const operators = document.querySelectorAll (".keys__orange_bg, #keys__percent");
+const operators = document.querySelectorAll (".keys__orange_bg");
 const ans = document.querySelector("#keys__equal");
+const percent = document.querySelector("#keys__percent")
 const ac = document.querySelector("#keys__ac");
 const polarity = document.querySelector("#keys__polarity");
 
@@ -16,14 +19,13 @@ buttons.forEach((button)=>{
     screen.innerHTML += number;
     });
 })
-
+//variables (values changes)
 let firstNum;
 let secondNum;
 let operation;
 let digit;
 
-
-//right-handside functions
+//first-numbers
 operators.forEach((operator)=>{
     operator.addEventListener("click", (event) =>{ 
         operation =event.target.innerHTML;
@@ -31,29 +33,30 @@ operators.forEach((operator)=>{
         screen.innerHTML= operation;
 })
 })
-
+//operatators
 ans.addEventListener ("click",() => {
     secondNum = screen.innerHTML;
 
-    //include decimals
+    //includes decimals
     let number1 = parseFloat(firstNum);
     let number2 = parseFloat(secondNum);
 
     switch (operation) { 
-        case ("÷"): //divide to 5 d.p
-            screen.innerHTML = (number1/number2).toFixed(5);
+        case ("÷"): //divide to 2 d.p
+            screen.innerHTML = (number1/number2).toFixed(2);
+            calc.innerHTML = number1 + " ÷ " + number2 + " = ";
             break;
         case "×": //multiply
             screen.innerHTML = (number1*number2);
+            calc.innerHTML = number1 + " × " + number2 + " = ";
             break;
         case "-": //minus
             screen.innerHTML = (number1-number2);
+            calc.innerHTML = number1 + " - " + number2 + " = ";
             break;
         case "+": //plus
             screen.innerHTML = (number1+number2);
-            break;
-        case "%": //percent
-            screen.innerHTML = (number1/100);
+            calc.innerHTML = number1 + " + " + number2 + " = ";
             break;
         default:
             screen.innerHTML = "error";
@@ -61,12 +64,23 @@ ans.addEventListener ("click",() => {
       }
 })
 
-ac.addEventListener("click", ()=>{
-    screen.innerHTML = ""
-    firstNum= 0;
-    secondNum= 0;
+percent.addEventListener("click", () => {
+    digit = screen.innerHTML;
+    let _digit = parseFloat(digit);
+
+    screen.innerHTML = (digit/100).toFixed(2);
 })
 
+//clear
+ac.addEventListener("click", ()=>{
+    screen.innerHTML = "";
+    calc.innerHTML = "";
+    firstNum =0;
+    secondNum =0;
+    digit =0;
+})
+
+//number polarity
 polarity.addEventListener("click", ()=>{
     digit = screen.innerHTML;
     let _digit = parseFloat(digit);
